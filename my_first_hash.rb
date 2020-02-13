d5d1c304bb88394aff22dcb2ba3225d78845961b
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-13 00:52:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
-def my_hash (my_hash)
+def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"Bass" => "G and L"}
+my_hash = {"Bass" => "G and L", "Bass 2" => "Ibanez"}
 
 end
 
